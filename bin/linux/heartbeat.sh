d14b2789d1dbc4f0ad2d2f71ae061d86dcb37b89
Shlomi Noach <shlomi-noach@users.noreply.github.com>
2020-04-12 11:26:10 +0300

    reduce heartbeat interval

--- bin/linux/heartbeat.sh
+++ bin/linux/heartbeat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while sleep 1 ; do
+while sleep 0.5 ; do
   mysql -uci -pci -h 127.0.0.1 --port 13306 -s -s -e "
     replace into test.heartbeat (id, ts) values (1, now(6))
   " || :
